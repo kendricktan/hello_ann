336389a9798c25ff3e76aa675372a86660407cec
kendricktan <kendricktan0814@gmail.com>
2016-10-13 21:23:50 +1000

    ✍ forward pass confirm working

--- src/main/scala/Network.scala
+++ src/main/scala/Network.scala
@@ -19,104 +19,121 @@ class Network(inputSize: Int, var layers: Seq[Layers]) {
   // dimensional array known as 'weight' to the array
   def addLayer(inputSize: Int): Unit = {
     // Construct a new layer and add a weight matrix to it
-    // Randomize weight valies to be between -1.25 and 2.5
-    val new_layer = new Layers(inputSize)
-    new_layer.weights = Array.ofDim[Double](layers.last.length, inputSize)
-    new_layer.weights = new_layer.weights.map(x => x.map(y => -1.25 + (Random.nextDouble() * 2.5)))
+    // Randomize weight valies to be between -1.25 and 1.25
+    this.layers.last.weights = Array.ofDim[Double](this.layers.last.length, inputSize)
+    this.layers.last.weights = this.layers.last.weights.map(x => x.map(y => 0.25 ))
+    //this.layers.last.weights = this.layers.last.weights.map(x => x.map(y => -1.25 + (Random.nextDouble() * 2.5)))
 
     // Create new layer and ammend it to network's layers
-    layers = layers ++ Seq(new_layer)
+    this.layers = this.layers ++ Seq(new Layers(inputSize))
   }
 
   // Will try and implement in matrix format if have time
   // Try and fit model to training data
   def fit(inputs: Array[Neurons], outputs: Array[Neurons]): Unit = {
-    // Set the first layer to be input
+    if (inputs.length != this.layers.head.neurons.length){
+      throw new IllegalArgumentException("Inputs must be of the same size!")
+    }
+
     this.layers.head.neurons = inputs
 
-    // Forward pass to store the outputs
-    // Go through each layer, except for input
-    for (i <- 1 until this.layers.length) {
-      // for each layer for through each of
-      // its neurons
-      for (y <- 0 until this.layers(i).neurons.length) {
-        // Variable to calculate the outputs
-        var c_output: Double = 0.0
+    // Propagate forward
+    for (i <- 0 until this.layers.length - 1) {
 
-        // Iterate through the output neurons
-        for (x <- 0 until this.layers(i - 1).neurons.length - 1) {
-          // Get output neuron
-          val o_neurons = this.layers(i - 1).neurons(x)
+      // Loop through next layer
+      for (y <- this.layers(i + 1).neurons.indices) {
+        // Neuron outputs to be stored
+        var c_outputs = 0.0
 
-          // Current weight
-          val c_weight = this.layers(i).weights(x)(y)
-
-          // If i = 0 then we're getting the output,
-          // not the activation from neuron
-          // since it's the input
-          if (i - 1 == 0) {
-            c_output = c_output + (o_neurons.output * c_weight)
+        // Loop through current layer
+        for (x <- this.layers(i).neurons.indices) {
+          // If input layer we just want the outputs, not the activation
+          if (i == 0) {
+            c_outputs = c_outputs + (this.layers(i).weights(x)(y) * this.layers(i).neurons(x).output)
           }
-          else {
-            c_output = c_output + (o_neurons.activation() * c_weight)
+
+          // Else we want the activation function
+          else{
+            c_outputs = c_outputs + (this.layers(i).weights(x)(y) * this.layers(i).neurons(x).activation())
           }
         }
 
-        // Got our output, now make that the input neuron's output
-        println(c_output)
-        this.layers(i).neurons(y).output = c_output
+        // Store it in the next layer's neuron
+        this.layers(i+1).neurons(y).output = c_outputs
       }
     }
 
-    // Backwards propagation
-    // Calculate the rate of change of each output with respect to
-    // the error
 
-    // To 1 instead of 0 because input layer doesn't have any
-    // weight matrix
-    for (i <- this.layers.length-1 to 1 by -1) {
-      // Back propagate through each neuron
-      for (x <- 0 until this.layers(i - 1).neurons.length) {
-        for (y <- 0 until this.layers(i).neurons.length) {
-          println(x, y, i)
-          // Back propagation has a different algorithm for
-          // the output layer weightings
+    // Backward propagation
+    for (i <- this.layers.length-1 to 1 by -1){
+      for (y <- this.layers(i-1).neurons.indices){
+        for (x <- this.layers(i).neurons.indices){
+          // If its a output layer we need to use a different algorithm
+          if (i == this.layers.length-1){
+            // Neuron activation
+            val n_a = this.layers(i).neurons(x).activation()
+            val theta_a = n_a * (1.0 - n_a) * (n_a - outputs(x).output)
 
-          // output layer algorithm: where l = last, and t_l = target
-          // o_l = o_l-1 * delta_l
-          // where delta_l = o_l*(1-o_l)*(o_l-t_l)
-
-          // if cur layer is output layer
-          if (this.layers(i) == this.layers.last) {
-            // Output neuron
-            val o_n = this.layers(i).neurons(y)
-
-            // Neuron gradient
-            val theta_n = o_n.activation() * (1.0 - o_n.activation()) * (o_n.activation() - outputs(y).activation())
-
-            this.layers(i).weights(x)(y) = this.layers(i).weights(x)(y) - (this.learning_rate * this.layers(i).neurons(y).activation() * theta_n)
+            // Change weighting of neuron
+            val theta_weight = -this.learning_rate*theta_a*this.layers(i-1).weights(y)(x)
+            this.layers(i-1).weights(y)(x) += theta_weight
           }
 
-          // hidden layer algorithm:
-          // o_h = o_h * delta_h
-          // where delta_h = o_h*(1-o_h)*sum(delta_h+1*w_h_h+1)
-          // where w_h_h+1 is the weight matrix from layer h to layer h+1
-          // if its an input layer  
-          else {
-            // Sum of the weights from the previous layer
-            // and their delta, used to compute current layer's gradient
-            val o_n = this.layers(i + 1).neurons
-            val o_n_a = o_n.map(a => a.activation())
-            val theta_n = (o_n_a, o_n_a.map(q => 1.0 - q), (o_n_a, outputs.map(a => a.activation())).zipped.map(_ - _)).zipped.map(_ * _ * _)
-            val sum_theta_n = (theta_n, this.layers(i + 1).weights(y)).zipped.map(_ * _).sum
+          else{
+            // We need data from previous layer (reversed) to
+            // compute the gradient of the current layer
+            // Neurons from the previous layer
+            val n_p = this.layers(i+1).neurons
+            val n_p_a = n_p.map(a => a.activation())
+            val theta_p_a = (n_p_a, n_p_a.map(a => 1.0 - a), (n_p_a, outputs.map(a=>a.output)).zipped.map(_-_)).zipped.map(_*_*_)
+            val theta_p_a_sum = theta_p_a.sum
 
-            // Current layer neurons and gradients
-            val c_n = this.layers(i).neurons(x)
-            val theta_c_n = c_n.activation() * (1.0 - c_n.activation()) * sum_theta_n
-            this.layers(i).weights(x)(y) = this.layers(i).weights(x)(y) - (this.learning_rate * this.layers(i).neurons(y).activation() * theta_c_n)
+            // Got values from previous layer,
+            // Now compute for current layer
+            val n_a = this.layers(i).neurons(x).activation()
+            val theta_a = n_a * (1.0 - n_a) * theta_p_a_sum
+
+            // Change weighting of neuron
+            val theta_weight = -this.learning_rate*theta_a*this.layers(i-1).weights(y)(x)
+            this.layers(i-1).weights(y)(x) += theta_weight
           }
         }
       }
     }
+
   }
+
+  def predict(inputs: Array[Neurons]) : Array[Double] = {
+    this.layers.head.neurons = inputs
+
+    // Propagate forward
+    for (i <- 0 until this.layers.length - 1) {
+
+      // Loop through next layer
+      for (y <- this.layers(i + 1).neurons.indices) {
+        // Neuron outputs to be stored
+        var c_outputs = 0.0
+
+        // Loop through current layer
+        for (x <- this.layers(i).neurons.indices) {
+
+          // If input layer we just want the outputs, not the activation
+          if (i == 0) {
+            c_outputs = c_outputs + (this.layers(i).weights(x)(y) * this.layers(i).neurons(x).output)
+          }
+
+          // Else we want the activation function
+          else{
+            c_outputs = c_outputs + (this.layers(i).weights(x)(y) * this.layers(i).neurons(x).activation())
+          }
+        }
+
+        // Store it in the next layer's neuron
+        this.layers(i+1).neurons(y).output = c_outputs
+      }
+    }
+
+    this.layers.last.neurons.map(a => a.activation())
+  }
+
 }
--- src/main/scala/hello_ann.scala
+++ src/main/scala/hello_ann.scala
@@ -8,25 +8,26 @@ object hello_ann {
     // Basic binary calculator
     // Do note that the input layers and output layers
     // defined here must be the same size
-    val inputs = Array[Double](1.0, 1.0, 1.0)
-    val outputs = Array[Double](7.0)
+    val inputs = Array[Double](1.0, 1.0)
+    val outputs = Array[Double](3.0)
 
     // Simple 3 layered ANN
     // With a hidden layer
     // Initialize our network
     // Input layer has 3 nodes
-    var network = new Network(3)
+    var network = new Network(2)
 
     // Hidden layer consists of 5 nodes
-    network.addLayer(5)
+    network.addLayer(3)
 
     // 1 node for output layer
     network.addLayer(1)
 
     // Fit network to this output
-    for (x <- 0 until 100) {
+    for (x <- 0 until 1) {
       println("Epoch: " + x)
       network.fit(inputs.map(x => new Neurons(x)), outputs.map(x => new Neurons(x)))
+      println("Prediction: " + network.predict(inputs.map(x => new Neurons(x))).mkString(" "))
     }
   }
 }
